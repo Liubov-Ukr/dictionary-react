d0d8f0ac553c64fc81f24b95ca08ec1e6379c401
Liubov-Ukr <liubovopr@gmail.com>
2022-10-02 15:43:11 -0200

    created footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./Search";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer>
+      This progect was coded by{" "}
+      <a
+        href="https://github.com/Liubov-Ukr"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Liubov Opryshchenko
+      </a>{" "}
+      and is{" "}
+      <a
+        href="https://github.com/Liubov-Ukr/dictionary-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        open-sourced on GitHub
+      </a>{" "}
+    </footer>
+  );
+}
